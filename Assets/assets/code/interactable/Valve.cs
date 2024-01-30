9dd28771b749a17f5ab63c2462211a8a7f9a5732
Moritz Scheuerle <68465911+Plixo2@users.noreply.github.com>
2024-01-30 23:40:15 +0100

    updated valve

--- Assets/assets/code/interactable/Valve.cs
+++ Assets/assets/code/interactable/Valve.cs
@@ -3,43 +3,56 @@ using UnityEngine;
 
 public class Valve : Interactable
 {
-    public Transform _transform;
-    public ParticleSystem _particleSystem; // Drag the particle system to be controlled by this valve in Unity Inspector.
+    // public Transform _transform;
+    public Animator Animator;
+
+    public ParticleSystem
+        _particleSystem; // Drag the particle system to be controlled by this valve in Unity Inspector.
+
     private bool isOpen = true;
-    
+
     new public void Start()
     {
-            print("Registering Valve");
-            States.AddValve(this);
-            print("Registering Interactable");
-            States.AddInteractable(this);
+        States.AddValve(this);
+        States.AddInteractable(this);
     }
+
     public void toggleValve()
     {
-        _transform.Rotate(0, 0, 90);
+        Animator.SetTrigger("Toggle");
+        // _transform.Rotate(0, 0, 90);
         //playRotateAnimation();   // To be coded later!!
-        if(isOpen)
+        if (isOpen)
         {
-            _particleSystem.enableEmission = false;
+            if (_particleSystem != null)
+            {
+                _particleSystem.gameObject.SetActive(false);
+            }
+
             isOpen = !isOpen;
             onClosedAllValves();
         }
         else
         {
-            _particleSystem.enableEmission = true;
+            if (_particleSystem != null)
+            {
+                _particleSystem.gameObject.SetActive(true);
+            }
+
             isOpen = !isOpen;
         }
     }
+
     private void onClosedAllValves()
     {
-        foreach(Valve valve in States.getValves())
+        foreach (Valve valve in States.getValves())
         {
-            if(valve.getOpen())
+            if (valve.getOpen())
             {
                 return;
             }
         }
-        
+
         States.allValvesClosedOnce = true;
         print("Level Completed, All Valves Closed!!");
 
@@ -48,7 +61,7 @@ public class Valve : Interactable
         You can code here what happens when all valves are closed.
         //unlockDoor();
         //completeLevel();
-        
+
         */
     }
 
