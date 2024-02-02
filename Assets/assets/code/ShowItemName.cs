030f9d163e1340e7afe408c61ba54ad5ba4944a3
Lars Heyer <heyer.lars@gmail.com>
2024-02-03 00:01:49 +0100

    refactor

--- Assets/assets/code/ShowItemName.cs
+++ Assets/assets/code/ShowItemName.cs
@@ -6,18 +6,11 @@ namespace assets.code
 {
 public class ShowItemName : MonoBehaviour
 {
-    [SerializeField] private float playerDistance = 2;
+    [SerializeField] private Vector3 playerDistance;
     [SerializeField] private Player player;
-    void Start()
-    {
-        this.transform.position = player.transform.position + new Vector3(0,playerDistance,0);
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        this.transform.position = player.transform.position + new Vector3(0,playerDistance,0);
+        this.transform.position = player.transform.position + playerDistance;
     }
 }
 }
